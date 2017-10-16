caab2c198aca513ef3418d40946349b403240cf1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-16 18:05:49 -0400

    initial code

---
+++ src/commit-message-install-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* eslint-env mocha */
+const commitMessageInstall = require('.')
+
+describe('commit-message-install', () => {
+  it('write this test', () => {
+    console.assert(commitMessageInstall, 'should export something')
+  })
+})
