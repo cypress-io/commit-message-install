9f54b7f7eea8c50a386734d1eea68cb3791b333d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-10-31 16:17:18 -0400

    start message body processing

--- src/commit-message-install-spec.js
+++ src/commit-message-install-spec.js
@@ -1,13 +1,21 @@
 'use strict'
 
 /* eslint-env mocha */
-const { getMessage } = require('.')
+const { getMessage, getJsonBlock } = require('.')
 const schemaShot = require('schema-shot')
+const la = require('lazy-ass')
+const is = require('check-more-types')
 
 describe('commit-message-install', () => {
   context('gets last commit message', () => {
     it('returns an object', () => {
       return schemaShot(getMessage())
+    })
+  })
+
+  context('getJsonBlock', () => {
+    it('is a function', () => {
+      la(is.fn(getJsonBlock))
     })
   })
 })
--- src/index.js
+++ src/index.js
@@ -6,4 +6,34 @@ function getMessage () {
   return ggit.lastCommitId().then(ggit.commitMessage)
 }
 
-module.exports = { getMessage }
+// parses given commit message text (body)
+// and finds json block (if any)
+/**
+  example:
+
+  this commit does this thing
+  ```json
+  {
+    "name": "foo"
+  }
+  ```
+  and more
+
+  returns in the example case object
+    {"name": "foo"}
+*/
+function getJsonBlock (message) {
+  if (!message) {
+    return
+  }
+  const jsonStarts = message.indexOf('```json')
+  if (jsonStarts === -1) {
+    return
+  }
+  const jsonEnds = message.indexOf('```', jsonStarts + 6)
+  if (jsonEnds === -1) {
+
+  }
+}
+
+module.exports = { getMessage, getJsonBlock }
